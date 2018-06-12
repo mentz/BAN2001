a357fc27b8f24df6d76fd7694a1922499698d1df
Lucas Litter Mentz <lucaslittermentz@gmail.com>
2018-06-12 13:57:29 -0300

    Início

---
+++ Tarefas/Serializabilidade/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    // Formato das entradas: (linhas)
+    // <operacao> <id_transacao> [<operando>]
+    // Exemplo:
+    // s 1 
+    // r 1 x
+    // w 1 x
+    // t 1
+    
+    return 0;
+}
